8be2f79212d34ccbdd3ea9497d21deef2bb8fa19
Stephen Lang <info@skl.me>
2019-05-31 19:25:33 +0100

    tObj and tAmb calcs ported

--- drivers/i2c/calipile_driver.go
+++ drivers/i2c/calipile_driver.go
@@ -2,6 +2,7 @@ package i2c
 
 import (
 	"fmt"
+	"math"
 
 	"gobot.io/x/gobot"
 )
@@ -10,58 +11,71 @@ const (
 	calipileDefaultSlaveAddress = 0x0C
 
 	// CaliPile (TM) ASIC Registers
-	calipileRegisterTPObject1            = 0x01 // r
-	calipileRegisterTPObject3            = 0x02 // r
-	calipileRegisterTPObject4            = 0x03 // r
-	calipileRegisterTPAmbient1           = 0x03 // r
-	calipileRegisterTPAmbient2           = 0x04 // r
-	calipileRegisterTPObjLP11            = 0x05 // r
-	calipileRegisterTPObjLP12            = 0x06 // r
-	calipileRegisterTPObjLP13            = 0x07 // r
-	calipileRegisterTPObjLP21            = 0x07 // r
-	calipileRegisterTPObjLP22            = 0x08 // r
-	calipileRegisterTPObjLP23            = 0x09 // r
-	calipileRegisterTPAmbLP31            = 0x10 // r
-	calipileRegisterTPAmbLP32            = 0x11 // r
-	calipileRegisterTPObjLP2Frozen1      = 0x12 // r
-	calipileRegisterTPObjLP2Frozen2      = 0x13 // r
-	calipileRegisterTPObjLP2Frozen3      = 0x14 // r
-	calipileRegisterTPPresence           = 0x15 // r
-	calipileRegisterTPMotion             = 0x16 // r
-	calipileRegisterTPAmbShock           = 0x17 // r
-	calipileRegisterInterruptStatus      = 0x18 // r (autoclear)
-	calipileRegisterChipStatus           = 0x19 // r
-	calipileRegisterSLP1                 = 0x20 // r/w
-	calipileRegisterSLP2                 = 0x20 // r/w
-	calipileRegisterSLP3                 = 0x21 // r/w
-	calipileRegisterTPPresenceThreshold  = 0x22 // r/w
-	calipileRegisterTPMotionThreshold    = 0x23 // r/w
-	calipileRegisterTPAmbShockThreshold  = 0x24 // r/w
-	calipileRegisterInterruptMask        = 0x25 // r/w
-	calipileRegisterCycleTimeForMotion   = 0x26 // r/w
-	calipileRegisterSRCSelectForPresence = 0x26 // r/w
-	calipileRegisterTPOTDirection        = 0x26 // r/w
-	calipileRegisterTimerInterrupt       = 0x27 // r/w
-	calipileRegisterTPOTThreshold1       = 0x28 // r/w
-	calipileRegisterTPOTThreshold2       = 0x29 // r/w
-	calipileRegisterEEPROMControl        = 0x31 // r/w - 0x80 to enable read, 0x00 to disable
+	calipileRegisterTPObject1            = 1  // r
+	calipileRegisterTPObject2            = 2  // r
+	calipileRegisterTPObject3            = 3  // r, bit 7 only
+	calipileRegisterTPAmbient1           = 3  // r, bits [6:0]
+	calipileRegisterTPAmbient2           = 4  // r
+	calipileRegisterTPObjLP11            = 5  // r
+	calipileRegisterTPObjLP12            = 6  // r
+	calipileRegisterTPObjLP13            = 7  // r
+	calipileRegisterTPObjLP21            = 7  // r
+	calipileRegisterTPObjLP22            = 8  // r
+	calipileRegisterTPObjLP23            = 9  // r
+	calipileRegisterTPAmbLP31            = 10 // r
+	calipileRegisterTPAmbLP32            = 11 // r
+	calipileRegisterTPObjLP2Frozen1      = 12 // r
+	calipileRegisterTPObjLP2Frozen2      = 13 // r
+	calipileRegisterTPObjLP2Frozen3      = 14 // r
+	calipileRegisterTPPresence           = 15 // r
+	calipileRegisterTPMotion             = 16 // r
+	calipileRegisterTPAmbShock           = 17 // r
+	calipileRegisterInterruptStatus      = 18 // r (autoclear)
+	calipileRegisterChipStatus           = 19 // r
+	calipileRegisterSLP1                 = 20 // r/w
+	calipileRegisterSLP2                 = 20 // r/w
+	calipileRegisterSLP3                 = 21 // r/w
+	calipileRegisterTPPresenceThreshold  = 22 // r/w
+	calipileRegisterTPMotionThreshold    = 23 // r/w
+	calipileRegisterTPAmbShockThreshold  = 24 // r/w
+	calipileRegisterInterruptMask        = 25 // r/w
+	calipileRegisterCycleTimeForMotion   = 26 // r/w
+	calipileRegisterSRCSelectForPresence = 26 // r/w
+	calipileRegisterTPOTDirection        = 26 // r/w
+	calipileRegisterTimerInterrupt       = 27 // r/w
+	calipileRegisterTPOTThreshold1       = 28 // r/w
+	calipileRegisterTPOTThreshold2       = 29 // r/w
+	calipileRegisterEEPROMControl        = 31 // r/w - 0x80 to enable read, 0x00 to disable
 
 	// CaliPile (TM) EEPROM Content Registers
-	calipileRegisterEEPROMProtocol     = 0x32
-	calipileRegisterEEPROMChecksum1    = 0x33
-	calipileRegisterEEPROMChecksum2    = 0x34
-	calipileRegisterEEPROMLookup       = 0x41
-	calipileRegisterEEPROMPTAT251      = 0x42
-	calipileRegisterEEPROMPTAT252      = 0x43
-	calipileRegisterEEPROMM1           = 0x44
-	calipileRegisterEEPROMM2           = 0x45
-	calipileRegisterEEPROMU01          = 0x46
-	calipileRegisterEEPROMU01          = 0x47
-	calipileRegisterEEPROMUOut11       = 0x48
-	calipileRegisterEEPROMUOut12       = 0x49
-	calipileRegisterEEPROMTObj1        = 0x50
-	calipileRegisterEEPROMSlaveAddress = 0x63 // r
+	calipileRegisterEEPROMProtocol     = 32
+	calipileRegisterEEPROMChecksum1    = 33
+	calipileRegisterEEPROMChecksum2    = 34
+	calipileRegisterEEPROMLookup       = 41
+	calipileRegisterEEPROMPTAT251      = 42
+	calipileRegisterEEPROMPTAT252      = 43
+	calipileRegisterEEPROMM1           = 44
+	calipileRegisterEEPROMM2           = 45
+	calipileRegisterEEPROMU01          = 46
+	calipileRegisterEEPROMU02          = 47
+	calipileRegisterEEPROMUOut11       = 48
+	calipileRegisterEEPROMUOut12       = 49
+	calipileRegisterEEPROMTObj1        = 50
+	calipileRegisterEEPROMSlaveAddress = 63 // r
 )
+
+type eeprom struct {
+	protocol     uint8
+	checksum     uint16
+	lookup       uint8
+	ptat25       uint16
+	m            uint16
+	u0           uint16
+	uOut1        uint32
+	tObj1        uint8
+	slaveAddress uint8
+	k            float64
+}
 
 // CalipileDriver is an I2C driver for the Excelitas CaliPile TPiS 1385 thermopile sensor
 type CalipileDriver struct {
@@ -69,6 +83,7 @@ type CalipileDriver struct {
 	connector  Connector
 	connection Connection
 	Config
+	eeprom *eeprom
 }
 
 // NewCalipileDriver creates a new driver with specified i2c interface.
@@ -77,6 +92,7 @@ func NewCalipileDriver(c Connector, options ...func(Config)) *CalipileDriver {
 		name:      gobot.DefaultName("CaliPile"),
 		connector: c,
 		Config:    NewConfig(),
+		eeprom:    &eeprom{},
 	}
 
 	for _, option := range options {
@@ -105,6 +121,18 @@ func (d *CalipileDriver) Connection() gobot.Connection {
 func (d *CalipileDriver) Start() (err error) {
 	bus := d.GetBusOrDefault(d.connector.GetDefaultBus())
 	address := d.GetAddressOrDefault(calipileDefaultSlaveAddress)
+
+	tmpConnection, err := d.connector.GetConnection(0x00, bus)
+	if err != nil {
+		return fmt.Errorf("CalipileDriver::initialization() failed to create connection to 0x00:\n\t%v", err)
+	}
+
+	// Issue general call (to slave address 0x00 from tmpConnection) and reload command
+	err = tmpConnection.WriteByteData(0x04, 0x00)
+	if err != nil {
+		return fmt.Errorf("CalipileDriver::initialization() failed to issue general call and reload command:\n\t%v", err)
+	}
+
 	if d.connection, err = d.connector.GetConnection(address, bus); err != nil {
 		return err
 	}
@@ -118,25 +146,183 @@ func (d *CalipileDriver) Halt() (err error) {
 
 // Verify comms link to slave device
 func (d *CalipileDriver) initialization() (err error) {
-	addr8, err := d.ReadAddress()
-	if err != nil {
-		return fmt.Errorf("CalipileDriver::initialization() failed to communicate with slave device:\n\t%v", err)
+	if err = d.ReadEEPROM(); err != nil {
+		return fmt.Errorf("CalipileDriver::initialization() failed to initialize device:\n\t%v", err)
 	}
 
-	addrExpected := d.GetAddressOrDefault(calipileDefaultSlaveAddress)
-	if int(addr8) != addrExpected {
-		return fmt.Errorf("CalipileDriver::initialization() SMBus slave address command returned mismatch, expected:%X actual:%X", addrExpected, int(addr8))
+	addrExpected := uint8(d.GetAddressOrDefault(calipileDefaultSlaveAddress))
+	if d.eeprom.slaveAddress != addrExpected {
+		return fmt.Errorf("CalipileDriver::initialization() I2C slave address mismatch, expected:%02X actual:%02X", addrExpected, d.eeprom.slaveAddress)
 	}
 
-	return nil
+	// See datasheet section 8.5
+	d.eeprom.k = (float64(d.eeprom.uOut1) - float64(d.eeprom.u0)) / (math.Pow(float64(d.eeprom.tObj1)+273.15, 3.8) - math.Pow(25.0+273.15, 3.8))
+
+	fmt.Println(d.eeprom)
+
+	return err
 }
 
-// ReadAddress returns the 7-bit I2C slave address from EEPROM
-func (d *CalipileDriver) ReadAddress() (uint8, error) {
-	val16, err := d.read(calipileRegisterEEPROMSlaveAddress)
+// ReadEEPROM returns the 7-bit I2C slave address from EEPROM
+func (d *CalipileDriver) ReadEEPROM() error {
+	// Enable EEPROM read access
+	d.connection.WriteByteData(calipileRegisterEEPROMControl, 0x80)
+
+	val8, err := d.connection.ReadByteData(calipileRegisterEEPROMProtocol)
 	if err != nil {
-		return 0, fmt.Errorf("CalipileDriver::ReadAddress() failed:\n\t%v", err)
+		return fmt.Errorf("CalipileDriver::ReadEEPROM() failed to read PROTOCOL:\n\t%v", err)
+	}
+	d.eeprom.protocol = val8
+
+	val8, err = d.connection.ReadByteData(calipileRegisterEEPROMSlaveAddress)
+	if err != nil {
+		return fmt.Errorf("CalipileDriver::ReadEEPROM() failed to read SLAVE ADD:\n\t%v", err)
+	}
+	d.eeprom.slaveAddress = val8 & 0x7F // Clear external addressing bit to retrieve 7-bit address
+
+	val8, err = d.connection.ReadByteData(calipileRegisterEEPROMLookup)
+	if err != nil {
+		return fmt.Errorf("CalipileDriver::ReadEEPROM() failed to read LOOKUP#:\n\t%v", err)
+	}
+	d.eeprom.lookup = val8
+
+	val8h, err := d.connection.ReadByteData(calipileRegisterEEPROMPTAT251)
+	if err != nil {
+		return fmt.Errorf("CalipileDriver::ReadEEPROM() failed to read PTAT25:\n\t%v", err)
+	}
+	val8l, err := d.connection.ReadByteData(calipileRegisterEEPROMPTAT252)
+	if err != nil {
+		return fmt.Errorf("CalipileDriver::ReadEEPROM() failed to read PTAT25:\n\t%v", err)
+	}
+	d.eeprom.ptat25 = uint16(val8h)<<8 | uint16(val8l)
+
+	val8h, err = d.connection.ReadByteData(calipileRegisterEEPROMM1)
+	if err != nil {
+		return fmt.Errorf("CalipileDriver::ReadEEPROM() failed to read M:\n\t%v", err)
+	}
+	val8l, err = d.connection.ReadByteData(calipileRegisterEEPROMM2)
+	if err != nil {
+		return fmt.Errorf("CalipileDriver::ReadEEPROM() failed to read M:\n\t%v", err)
+	}
+	d.eeprom.m = uint16(val8h)<<8 | uint16(val8l)
+	d.eeprom.m /= 100 // M = RegVal / 100
+
+	val8h, err = d.connection.ReadByteData(calipileRegisterEEPROMU01)
+	if err != nil {
+		return fmt.Errorf("CalipileDriver::ReadEEPROM() failed to read U0:\n\t%v", err)
+	}
+	val8l, err = d.connection.ReadByteData(calipileRegisterEEPROMU02)
+	if err != nil {
+		return fmt.Errorf("CalipileDriver::ReadEEPROM() failed to read U0:\n\t%v", err)
+	}
+	d.eeprom.u0 = uint16(val8h)<<8 | uint16(val8l)
+	d.eeprom.u0 += 32768 // U0 = RegVal + 32768
+
+	val8h, err = d.connection.ReadByteData(calipileRegisterEEPROMUOut11)
+	if err != nil {
+		return fmt.Errorf("CalipileDriver::ReadEEPROM() failed to read Uout1:\n\t%v", err)
+	}
+	val8l, err = d.connection.ReadByteData(calipileRegisterEEPROMUOut12)
+	if err != nil {
+		return fmt.Errorf("CalipileDriver::ReadEEPROM() failed to read Uout1:\n\t%v", err)
+	}
+	d.eeprom.uOut1 = uint32(val8h)<<8 | uint32(val8l)
+	d.eeprom.uOut1 *= 2 // Uout1 = RegVal * 2
+
+	val8, err = d.connection.ReadByteData(calipileRegisterEEPROMTObj1)
+	if err != nil {
+		return fmt.Errorf("CalipileDriver::ReadEEPROM() failed to read Tobj1:\n\t%v", err)
+	}
+	d.eeprom.tObj1 = val8
+
+	// Disable EEPROM read access
+	d.connection.WriteByteData(calipileRegisterEEPROMControl, 0x00)
+
+	return err
+}
+
+// ReadRawTPObject returns the 17-bit raw ADC value of TPobject
+func (d *CalipileDriver) ReadRawTPObject() (tpObjectRaw uint32, err error) {
+	val8a, err := d.connection.ReadByteData(calipileRegisterTPObject1)
+	if err != nil {
+		return 0, fmt.Errorf("CalipileDriver::ReadRawTPObject() failed:\n\t%v", err)
+	}
+	val8b, err := d.connection.ReadByteData(calipileRegisterTPObject2)
+	if err != nil {
+		return 0, fmt.Errorf("CalipileDriver::ReadRawTPObject() failed:\n\t%v", err)
+	}
+	val8c, err := d.connection.ReadByteData(calipileRegisterTPObject3)
+	if err != nil {
+		return 0, fmt.Errorf("CalipileDriver::ReadRawTPObject() failed:\n\t%v", err)
 	}
 
-	return uint8(val16), nil
+	// Read 17-bit value
+	tpObjectRaw = ((uint32(val8a) << 24) | (uint32(val8b) << 16) | (uint32(val8c&0x80) << 8)) >> 15
+
+	return tpObjectRaw, err
+}
+
+// ReadRawTPAmbient returns the 15-bit raw ambient temperature sensor value (PTAT)
+func (d *CalipileDriver) ReadRawTPAmbient() (tpAmbientRaw uint16, err error) {
+	val8h, err := d.connection.ReadByteData(calipileRegisterTPAmbient1)
+	if err != nil {
+		return 0, fmt.Errorf("CalipileDriver::ReadRawTPAmbient() failed:\n\t%v", err)
+	}
+	val8l, err := d.connection.ReadByteData(calipileRegisterTPAmbient2)
+	if err != nil {
+		return 0, fmt.Errorf("CalipileDriver::ReadRawTPAmbient() failed:\n\t%v", err)
+	}
+
+	// Read 15-bit value
+	tpAmbientRaw = (uint16(val8h&0x7F) << 8) | uint16(val8l)
+
+	return tpAmbientRaw, err
+}
+
+// CalculateTPAmbient returns the Calculated Ambient Temperature in degrees Kelvin
+func (d *CalipileDriver) CalculateTPAmbient() (tpAmbient float64, err error) {
+	amb16, err := d.ReadRawTPAmbient()
+	if err != nil {
+		return 0, fmt.Errorf("CalipileDriver::CalculateTPAmbient() failed:\n\t%v", err)
+	}
+
+	// Tamb [K] = (25 + 273.15) + (TPambient − PTAT25) · (1/M)
+	tpAmbient = 298.15 + (float64(amb16)-float64(d.eeprom.ptat25))*(1.0/float64(d.eeprom.m))
+
+	return tpAmbient, err
+}
+
+// CalculateTPAmbientC returns TPAmbient in degrees Celcius
+func (d *CalipileDriver) CalculateTPAmbientC() (tpAmbient float64, err error) {
+	tpAmbient, err = d.CalculateTPAmbientC()
+	tpAmbient -= 273.15
+
+	return tpAmbient, err
+}
+
+// CalculateTPObject returns the Calculated Object Temperature in degres Kelvin
+func (d *CalipileDriver) CalculateTPObject() (tpObject float64, err error) {
+	obj32, err := d.ReadRawTPObject()
+	if err != nil {
+		return 0, fmt.Errorf("CalipileDriver::CalculateTPObject() failed:\n\t%v", err)
+	}
+
+	tpAmbient, err := d.CalculateTPAmbient()
+	if err != nil {
+		return 0, fmt.Errorf("CalipileDriver::CalculateTPObject() failed:\n\t%v", err)
+	}
+
+	temp0 := math.Pow(tpAmbient, 3.8)
+	temp1 := (float64(obj32) - float64(d.eeprom.u0)) / d.eeprom.k
+	tpObject = math.Pow(temp0+temp1, 0.2631578947)
+
+	return tpObject, err
+}
+
+// CalculateTPObjectC returns TPObject in degrees Celcius
+func (d *CalipileDriver) CalculateTPObjectC() (tpObject float64, err error) {
+	tpObject, err = d.CalculateTPObject()
+	tpObject -= 273.15
+
+	return tpObject, err
 }
